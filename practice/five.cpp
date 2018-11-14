967c2bb975731dfca8b9327f38aee0759beaba0e
Lambda1 <breakthrough.dodorara@gmail.com>
2018-11-14 15:35:21 +0900

    as

--- practice/five.cpp
+++ practice/five.cpp
@@ -2,10 +2,24 @@
 
 int main(int argc,char *argv[])
 {
-	int a,b,c;
+	int a,b,c,x;
+	int m,cnt = 0;
 
-	std::cin >> a >> b >> c;
+	std::cin >> a >> b >> c >> x;
 
+	for(int i = 0;i <= a;i++)
+		for(int j = 0;j <= b;j++){
+			for(int k = 0;k <= c;k++){
+				m = (500*i + 100*j + 50*k);
+				if(m > x) break;
+				if(m == x){
+					cnt++;
+					break;
+				}
+			}
+		}
+
+	std::cout << cnt << std::endl;
 
 	return 0;
 }
