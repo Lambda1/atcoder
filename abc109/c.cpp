f4b4f99d26e16fd26a4df4d995bdc718b0db7c83
lambda1 <breakthrough.dodorara@gmail.com>
2018-11-21 14:48:05 +0900

    lose c

--- abc109/c.cpp
+++ abc109/c.cpp
@@ -5,21 +5,30 @@
 #include <cmath>
 #include <map>
 
+#define MIN(a,b) ((a<b)?a:b)
+#define MAX(a,b) ((a>b)?a:b)
+
+int gcd(int a,int b)
+{
+	if(!b) return a;
+	return gcd(b,a%b);
+}
+
 int main(int argc,char *argv[])
 {
 	int n,x;
-	std::cin >> n;
-	n++;
+	std::cin >> n >> x;
 
 	std::vector<int> v(n);
-	for(int i = 0;i < n;i++) std::cin >> v[i];
+	for(int i = 0;i < n;i++){
+		std::cin >> v[i];
+		v[i] = abs(v[i]-x);
+	}
+	
+	int d = v[0];
+	for(int i = 1;i < n;i++) d = gcd(MAX(d,v[i]),MIN(d,v[i]));
 
-	std::vector<int> way;
-	for(int i = 0;i < n;i++)
-		for(int j = 0;j < n;j++) way.push_back(abs(v[i]-v[j]));
-
-	for(int i = 0;i < way.size();i++)
-	std::cout << way[i] << std::endl;
+	std::cout << d << std::endl;
 
 	return 0;
 }
