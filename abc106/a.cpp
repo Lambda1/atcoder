c539ff512538bc61c3d255e4c736ea2db806bbd9
Lambda1 <breakthrough.dodorara@gmail.com>
2018-11-27 09:12:21 +0900

    a

---
+++ abc106/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+
+int main(int argc,char *argv[])
+{
+	int a,b;
+	std::cin >> a >> b;
+	std::cout << a*b - a - b + 1 << std::endl;
+	return 0;
+}
