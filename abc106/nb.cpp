2aee69e8aa8cbce9a01037cd06a9037b6429c985
Lambda1 <breakthrough.dodorara@gmail.com>
2018-11-27 20:35:03 +0900

    add x

--- abc106/nb.cpp
+++ abc106/nb.cpp
@@ -5,36 +5,30 @@
 #include <cmath>
 #include <map>
 
-void Sieve_of_Eratosthenes(int n,std::vector<int> *s)
+void Sieve_of_Eratosthenes(int n,int *v)
 {
-	int *v = new int[201];
 	for(int i = 0;i < n;i++) v[i] = 0;
 
 	v[0] = v[1] = 1;
-	for(int i = 2;i < sqrt(n);i++)
-		for(int j = 2;i*j < n;j++) v[i*j] = 1;
+	for(int i = 2;i <= n;i++)
+		for(int j = 1;i*j <= n;j++) v[i*j]++;
 	
-	for(int i = 0;i < n;i++)
-		if(!v[i]) s->push_back(i);
-	
-	delete [] v;
+	int cnt = 0;
+	for(int i = 1;i <= n;i+=2){
+		//if(v[i]+1 == 8) cnt++;
+		//std::cout << i << ":" << v[i] << "  ";
+		if((i+1)%10 == 0) std::cout << std::endl;
+	}
+	std::cout << cnt << std::endl;
 }
 
 int main(int argc,char *argv[])
 {
 	int n;
-	int s[201] = {};
-	std::vector<int> v;
+	int v[201] = {};
 	std::cin >> n;
 
-	Sieve_of_Eratosthenes(n,&v);
+	Sieve_of_Eratosthenes(n,v);
 
-	for(int i = 0;i < v.size();i++){
-		if(!(n%v[i])){
-			s[v[i]]++;
-			n /= v[i];
-			i = -1;
-		}
-	}
 	return 0;
 }
