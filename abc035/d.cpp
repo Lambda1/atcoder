1c73057733f3ce2c2f31a65a61fe21c52ef337e6
lambda1_chiba <breakthrough.dodorara@gmail.com>
2019-10-16 13:43:06 +0900

    何回やってもコーナケースが解けなかった.

--- abc035/d.cpp
+++ abc035/d.cpp
@@ -1,55 +1,79 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <cmath>
-#include <map>
+#include <list>
+#include <queue>
+#include <utility>
 
-template <class T>
-class Edge
+constexpr int MAX_COST = 99999;
+
+template<class T>
+struct edge
 {
-	T m_to;
-	T m_weight;
-	public:
-	Edge() : m_to(0), m_weight(0){}
-	Edge(const T to,const T weight) : m_to(to), m_weight(weight) {}
-	~Edge(){}
-	inline void Init(const T to,const T weight){ m_to = to, m_weight = weight; }
-	inline T GetTo() const { return m_to; }
-	inline T GetWeight() const { return m_weight; }
+	long long int to;
+	T cost;
+	edge() : to(0), cost(static_cast<T>(0)) {}
+	edge(const long long int &n_v, const T &val) : to(n_v), cost(val) {}
+	~edge() {}
 };
-using Graph = std::vector<std::vector<Edge<int>>>;
+
+	template<class T>
+std::vector<T> dijkstra(std::vector<std::vector<edge<T>>> graph)
+{
+	std::vector<T> weight(static_cast<long long int>(graph.size()),MAX_COST);
+	weight[0] = 0;
+
+	std::priority_queue<std::pair<T,long long int>,std::vector<std::pair<T,long long int>>,std::greater<std::pair<T,long long int>>> pq;
+	pq.push(std::make_pair(weight[0], 0));
+
+	while(!pq.empty())
+	{
+		T cost = pq.top().first;
+		long long int v = pq.top().second;
+		pq.pop();
+		if(weight[v] < cost) continue;
+		for(long long int i = 0;i < static_cast<long long int>(graph[v].size());i++)
+		{
+			edge<T> now = graph[v][i];
+			if(weight[now.to] > now.cost + weight[v])
+			{
+				weight[now.to] = now.cost + weight[v];
+				pq.push(std::make_pair(weight[now.to], now.to));
+			}
+		}
+	}
+
+	return weight;
+}
 
 int main(int argc,char *argv[])
 {
-	int n,m,t;
+	long long int n,m,t;
 	std::cin >> n >> m >> t;
 
-	std::vector<int> money;
-	for(int i = 0;i < n;i++)
-	{
-		int tmp;
-		std::cin >> tmp;
-		money.push_back(tmp);
-	}
+	std::vector<long long int> weight(n);
+	for(long long int i = 0;i < n;i++) std::cin >> weight[i];
 
-	Graph g(n);
-	for(int i = 0;i < m;i++)
+	std::vector<std::vector<edge<long long int>>> array(n), back(n);
+	for(long long int i = 0;i < m;i++)
 	{
-		int a,b,c;
+		long long int a,b,c;
 		std::cin >> a >> b >> c;
-		g[a-1].push_back(Edge<int>(b,c));
+
+		array[a-1].push_back(edge<long long int>(b-1, c));
+		back[b-1].push_back(edge<long long int>(a-1, c));
 	}
 
-	for(int i = 0;i < n;i++)
+	std::vector<long long int> costA = dijkstra<long long int>(array);
+	std::vector<long long int> costB = dijkstra<long long int>(back);
+
+	long long int cost = 0;
+	for(long long int i = 0;i < n;i++)
 	{
-		std::cout << i << " -->";
-		for(auto first = g[i].begin();first != g[i].end();first++)
-		{
-			std::cout << " " << first->GetTo();
-		}
-		std::cout << std::endl;
+		if(costA[i] >= MAX_COST || costB[i] >= MAX_COST) continue;
+		long long int tmp = (t - costA[i] - costB[i]) * weight[i];
+		cost = (cost < tmp) ? tmp : cost;
 	}
+	std::cout << cost << std::endl;
 
 	return 0;
 }
--- algorithm/dijkstra.cpp
+++ algorithm/dijkstra.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <list>
+#include <queue>
+
+template<class T>
+struct edge
+{
+	int to;
+	T cost;
+	edge() : to(0), cost(static_cast<T>(0)) {}
+	edge(const int &n_v, const T &val) : to(n_v), cost(val) {}
+	~edge() {}
+};
 
 template<class T>
 void Printer(const T *data,const int &n,const int &m)
@@ -17,7 +29,7 @@ void Printer(const T *data,const int &n)
 	std::cout << std::endl;
 }
 
-/* 隣接行列(最深部がゴールver) */
+/* 隣接行列 */
 template<class T>
 std::vector<T> dijkstra(const std::vector<T> &graph,const int &n)
 {
@@ -27,19 +39,56 @@ std::vector<T> dijkstra(const std::vector<T> &graph,const int &n)
 	for(int i = 0;i < n;i++) weight.push_back(static_cast<T>(MAX_COST));
 	weight[0] = 0;
 
-	for(int i = 0;i < n;i++)
+	std::queue<int> que;
+	que.push(0);
+
+	while(!que.empty())
 	{
-		for(int j = 0;j < n;j++)
+		int index = que.front();
+		que.pop();
+		for(int i = 0;i < n;i++)
 		{
-			if(graph[i*n+j] == static_cast<T>(ERR_COST)) continue;
-			
-			if((weight[i] + graph[i*n+j]) < weight[j])
+			if(graph[index*n+i] == static_cast<T>(ERR_COST)) continue;
+			T cost = weight[index] + graph[index*n+i];
+			if(cost < weight[i])
 			{
-				weight[j] = weight[i] + graph[i*n+j];
-				if(j < i){ i = j-1; break; }
+				weight[i] = cost;
+				que.push(i);
 			}
 		}
 	}
+
+	return weight;
+}
+
+template<class T>
+std::vector<int> dijkstra(std::vector<std::vector<edge<int>>> graph)
+{
+	const int n = graph.size();
+	constexpr int MAX_COST = 99999;
+	constexpr int ERR_COST = -1;
+	std::vector<T> weight;
+	for(int i = 0;i < n;i++) weight.push_back(static_cast<T>(MAX_COST));
+	weight[0] = 0;
+
+	std::queue<int> que;
+	que.push(0);
+	
+	while(!que.empty())
+	{
+		int index = que.front();
+		que.pop();
+		for(int i = 0;i < static_cast<int>(graph[index].size());i++)
+		{
+			edge<T> now = graph[index][i];
+			if(now.cost + weight[index] < weight[now.to])
+			{
+				weight[now.to] = now.cost + weight[index];
+				que.push(now.to);
+			}
+		}
+	}
+
 	return weight;
 }
 
@@ -53,12 +102,13 @@ int main(int argc,char *argv[])
 	for(int i = 0;i < n;i++)
 	{
 		std::cin >> weight[i];
-		if(weight[max] < weight[i]) max = i;
+		if(weight[max] < weight[i] && weight[i] != t) max = i;
 	}
 
+	/* 隣接行列ver */
+	/*
 	std::vector<int> array,reverse;
 	for(int i = 0;i < n*n;i++) array.push_back(-1), reverse.push_back(-1);
-
 	for(int i = 0;i < m;i++)
 	{
 		int a,b,c;
@@ -67,20 +117,22 @@ int main(int argc,char *argv[])
 		array[(a-1)*n+(b-1)] = c;
 		reverse[(b-1)*n+(a-1)] = c;
 	}
-
-	//Printer<int>(dijkstra<int>(array,n).data(),n);
-	Printer<int>(dijkstra<int>(reverse,n).data(),n);
-	/*
-	std::vector<int> data_x =
-	{
-		-1, 10,  5, -1, -1, -1,
-		10, -1,  2,  8, -1, -1,
-		 5,  2, -1, -1, 10, -1,
-		-1,  8, -1, -1, -1,  8,
-		-1, -1, 10, -1, -1, 10,
-		-1, -1, -1,  8, 10, -1
-	};
-	Printer<int>(dijkstra<int>(data_x,6).data(),6);
+	std::cout << (t - (dijkstra<int>(array,n)[max] + dijkstra<int>(reverse,n)[max])) * weight[max] << std::endl;
 	*/
+
+	std::vector<std::vector<edge<int>>> array(n), back(n);
+	for(int i = 0;i < m;i++)
+	{
+		int a,b,c;
+		std::cin >> a >> b >> c;
+		
+		array[a-1].push_back(edge<int>(b-1, c));
+		back[b-1].push_back(edge<int>(a-1, c));
+	}
+
+	int cost = (t - (dijkstra<int>(array)[max] + dijkstra<int>(back)[max])) * weight[max];
+
+	std::cout << cost << std::endl;
+
 	return 0;
 }
