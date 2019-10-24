70eb432c7995b950f7b50195a9c023a936141e9d
lambda1 <breakthrough.dodorara@gmail.com>
2019-10-24 15:11:47 +0900

    add d

--- abc143/d.cpp
+++ abc143/d.cpp
@@ -22,16 +22,13 @@ struct edge
 template<class T>
 T my_binary_search(const int &min,const int &max,const T &key,const std::vector<T> &data)
 {
-	const int mid = (max - min) >> 1;
+	const int mid = (max - min)/2 + min;
 
-	std::cout << mid << std::endl;
+	if(min > max) return -1;
 
 	if(data[mid] == key) return mid;
-	if(data[mid] > key)  return my_binary_search<T>(min,mid,key,data);
-
-	if(min == max) return -1;
-	
-	return -1;
+	if(data[mid] < key)  return my_binary_search<T>(mid+1,max,key,data);
+	return my_binary_search<T>(min,mid-1,key,data);
 }
 
 int main(int argc,char *argv[])
@@ -41,12 +38,21 @@ int main(int argc,char *argv[])
 
 	std::vector<lint> l(n);
 	for(int i = 0;i < n;i++) std::cin >> l[i];
-	//std::sort(l.begin(),l.end(),std::greater<lint>());
-	std::sort(l.begin(),l.end());
+	std::sort(l.begin(),l.end(),std::greater<lint>());
 
-	for(int i = 0;i < n;i++) std::cout << l[i] << std::endl;
+	int num = 0;
+	for(int a = 0;a < n;a++)
+	{
+		for(int b = a+1;b < n;b++)
+		{
+			for(int c = a+1;c < b;c++)
+			{
+				if(l[a] < l[b] + l[c] && l[b] < l[c]+l[a]) num++;
+			}
+		}
+	}
 
-	std::cout << my_binary_search<lint>(0,n,704,l) << std::endl;
+	std::cout << num << std::endl;
 
 	return 0;
 }
