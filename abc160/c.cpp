b42486d814c5cfd9551697d7e8d46d3089e3e5f8
U-LAMBDA\break_000 <breakthrough.dodorara@gmail.com>
2020-05-12 12:55:34 +0900

    abc161/160 c

--- abc160/c.cpp
+++ abc160/c.cpp
@@ -14,12 +14,28 @@ using ul = unsigned long long int;
 namespace lamlib
 {
 	// Absolute
-	template<class T> inline T abs(const T &a){ return (a>0) ? a : -a; }
+	template<class T>
+	inline T abs(const T &a){ return (a>0) ? a : -a; }
 	// GCD: a > b
 	ul gcd(ul a,ul b) { return (!b) ? a : gcd(b,a%b); }
 }
 
 int main(int argc,char *argv[])
 {
+	ll k,n;
+	std::cin >> k >> n;
+
+	std::vector<ll> a(n,0);
+	for(ll i = 0;i < n;++i) { std::cin >> a[i]; }
+	
+	ll max = (a[0]+k)-a[n-1];
+	for(ll i = 0;i < n-1;++i)
+	{
+		ll d = (a[i+1]-a[i]);
+		if(d > max){ max = d; }
+	}
+
+	std::cout << k-max << std::endl;
+
 	return 0;
 }
