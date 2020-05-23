a93d04e7757046a30bb46cef90af87b7e8d84f43
lambda1 <breakthrough.dodorara@gmail.com>
2020-05-23 22:25:54 +0900

    unchi

--- abc157/c.cpp
+++ abc157/c.cpp
@@ -24,23 +24,28 @@ int main(int argc,char *argv[])
 	ll n,m;
 	std::cin >> n >> m;
 
-	std::vector<ll> arr(n,10);
+	bool out = false;
+	std::vector<ll> arr(n,-1);
 	for(ll i = 0;i < m;++i)
 	{
 		ll s, c;
 		std::cin >> s >> c;
-		if(arr[s-1] > c) arr[s-1] = c;
+		if(arr[s-1] == -1) arr[s-1] = c;
+		else if(arr[s-1] != c) out = true;
 	}
 
-	if(arr[0] == 10 || arr[0] == 0) std::cout << -1 << std::endl;
-	else
+	if(n != 1 && arr[0] == 0) out = true;
+	if(out){ std::cout << -1 << std::endl; return 0; }
+
+	if(n == 1 && m == 0){ std::cout << 0 << std::endl; return 0; }
+
+	for(ll i = 0;i < n;++i)
 	{
-		for(ll i = 0;i < n;++i)
-		{
-			if(arr[i] == 10) std::cout << 0;
-			else std::cout << arr[i];
-		}
-		std::cout << std::endl;
+		if(i == 0 && arr[i] == -1) std::cout << 1;
+		else if(arr[i] == -1) std::cout << 0;
+		else std::cout << arr[i];
 	}
+	std::cout << std::endl;
+
 	return 0;
 }
