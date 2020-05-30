7d1b8a87f13ad7bb936c4a0f78fa7dfc8a9153d8
lambda1 <breakthrough.dodorara@gmail.com>
2020-05-30 14:57:00 +0900

    jsc2019 辞め

--- jsc2019/b.cpp
+++ jsc2019/b.cpp
@@ -29,5 +29,22 @@ namespace lamlib
 
 int main(int argc,char *argv[])
 {
+	ul n,k;
+	std::cin >> n >> k;
+
+	std::vector<ll> a(n,0);
+	for(ll i = 0;i < n;++i) std::cin >> a[i];
+
+	ul ans = 0;
+	for(ll i = 0;i < n;++i)
+	{
+		for(ll j = i+1;j < n;++j)
+		{
+			if(a[i] > a[j]) { ++ans; }
+		}
+	}
+
+	std::cout << ans*(k+1) << std::endl;
+
 	return 0;
 }
