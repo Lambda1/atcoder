e43862cfb04747b13d60875538dc1d8f198c6e47
lambda1_letsnote <breakthrough.dodorara@gmail.com>
2020-06-22 14:48:54 +0900

    first a

--- project_euler/problem0001.cpp
+++ project_euler/problem0001.cpp
@@ -76,5 +76,11 @@ namespace lamlib
 
 int main(int argc,char *argv[])
 {
+	ll ans = 0;
+
+	for(int i = 1;i < 1000;++i)
+		if(i%3 == 0 || i%5 == 0) ans += i;
+	std::cout << ans << std::endl;
+
 	return 0;
 }
