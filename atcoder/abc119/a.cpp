a4a57ddc21cc29b8b795173630e1d07db4abb559
lambda1 <breakthrough.dodorara@gmail.com>
2021-05-11 21:41:54 +0900

    abc119 a-b

--- atcoder/abc119/a.cpp
+++ atcoder/abc119/a.cpp
@@ -1,20 +1,86 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
 #include <algorithm>
 #include <cmath>
 #include <map>
-#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <bitset>
+#include <limits>
+#include <numeric>
+
+using ld = long double;
+using ll = long long int;
+using ul = unsigned long long int;
+
+namespace lamlib
+{
+	/* constant */
+	constexpr double epsilon = std::numeric_limits<double>::epsilon();
+	constexpr double pi = std::acos(-1.0);
+
+	/* math */
+	template<class T> inline T abs(const T &a){ return (a>0) ? a : -a; }
+	ul inline digit(const ul &num){ return static_cast<ul>(std::log10(num+epsilon))+1; }
+
+	/* algorithm */
+	ul gcd(const ul &a,const ul &b) { return (!b) ? a : gcd(b,a%b); }
+	ul lcm(const ul &a,const ul &b) { return (a/gcd(a,b))*b; } // a/gcd(a,b)*b <=> (a*b)/gcd(a,b)
+	std::vector<bool> eratosthenes(const ul &n)
+	{
+		std::vector<bool> prime_candidate(n,true);
+		prime_candidate[0] = prime_candidate[1] = false;
+		for(ul i = 2;i*i < n;++i)
+		{
+			if(!prime_candidate[i]) continue;
+			for(ul j = 2;i*j < n;++j) prime_candidate[i*j] = false;
+		}
+		return prime_candidate;
+	}
+	ll rho_method(const ll &n)
+	{
+		auto f = [&](const ll &xi){ return (xi*xi+1)%n; };
+		ll x = 2, y = 2, d = 1;
+		while(d == 1)
+		{
+			x = f(x);
+			y = f(f(y));
+			d = lamlib::gcd(lamlib::abs(x-y),n);
+		}
+		return d;
+	}
+	// 素因数分解(試し割)
+	std::vector<ll> prime_factorization_trial(const ll &n)
+	{
+		ll num = n;
+		std::vector<ll> prime;
+		for(ll i = 2;i*i <= n;++i)
+		{
+			while((num%i) == 0)
+			{
+				prime.emplace_back(i);
+				num /= i;
+			}
+		}
+		if(num > 1) prime.emplace_back(num);
+		return prime;
+	}
+
+	/* string */
+	inline ul same_char_count(const std::string s,const char &ch){ return std::count(std::cbegin(s),std::cend(s),ch); }
+}
+
+// std::cout << std::fixed << std::setprecision(15) << std::endl;
 
 int main(int argc,char *argv[])
 {
-	std::string str;
-	std::cin >> str;
+	int a, b, c;
+	std::cin >> a >> b >> c;
 
-	if(atoi(str.substr(0,4).c_str()) > 2018 && atoi(str.substr(5,7).c_str()) > 4)
-		std::cout << "TBD" << std::endl;
-	else std::cout << "Heisei" << std::endl;
-	//if(atoi(str.substr(8,11).c_str()))
+	if ((a*a + b*b) < c*c) std::cout << "Yes" << std::endl;
+	else std::cout << "No" << std::endl;
 
 	return 0;
 }
